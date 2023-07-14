4234e2bb26e6d4056584348f525a2198ce3462c2
Simran Singh <siimransiingh08@gmail.com>
2023-07-14 21:43:08 +0530

    minor change

--- frontend/src/pages/CheckoutForm.js
+++ frontend/src/pages/CheckoutForm.js
@@ -61,7 +61,7 @@ export default function CheckoutForm() {
       elements,
       confirmParams: {
         // Make sure to change this to your payment completion page
-        return_url: `https://ecommerce-green-chi.vercel.app/order-success/${currentOrder.id}`,
+        return_url: `https://ecommerce-green-chi.vercel.app/order-success/my-orders`,
       },
     });
 
