c8642a5992bf1c1c78f184e1245b72ae65a4f6c5
Simran Singh <siimransiingh08@gmail.com>
2023-07-14 17:48:11 +0530

    stripe checkout form configured

--- backend/index.js
+++ backend/index.js
@@ -74,7 +74,7 @@ opts.secretOrKey = process.env.JWT_SECRET_KEY;
 
 //middlewares
 
-server.use(express.static(path.resolve(__dirname, 'build')));
+server.use(express.static(('build')));
 server.use(cookieParser());
 server.use(
   session({
